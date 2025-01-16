0802fbfe907508f18f35663db14ef1f3511e6597
MarkoG-M <marko.gudelj-marjanovic@outlook.de>
2025-01-16 19:27:00 +0100

    finished

--- homepage/js/homepage.js
+++ homepage/js/homepage.js
@@ -1,9 +1,8 @@
 const videos = [
+    "videos/video4.mp4",
     "videos/video2.mp4",
     "videos/video3.mp4",
-    "videos/video4.mp4",
     "videos/video5.mp4",
-    "videos/video6.mp4",
     "videos/video7.mp4"
 ];
 
